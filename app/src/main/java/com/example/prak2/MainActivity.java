9f7579c738ae9a78ddfd055cef9c4b243911eda1
nandaqolivasari <nandaqolivasari@gmail.com>
2021-06-20 03:37:29 +0700

    first commit

--- app/src/main/java/com/example/prak2/MainActivity.java
+++ app/src/main/java/com/example/prak2/MainActivity.java
@@ -4,24 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttondua;
+    EditText A, B;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttondua = findViewById(R.id.button);
 
-        buttondua.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            }
-        });
+        A = (EditText) findViewById(R.id.editTIMA);
+        B = (EditText) findViewById(R.id.editTIMB);
+
+
+
     }
+
+    public void button(View view) {
+
+        Intent skuy = new Intent( this, MainActivity2.class);
+        skuy.putExtra("editTIMA", A.getText().toString());
+        skuy.putExtra("editTIMB", B.getText().toString());
+        startActivity(skuy);
+    }
+
+
 }
--- app/src/main/java/com/example/prak2/MainActivity2.java
+++ app/src/main/java/com/example/prak2/MainActivity2.java
@@ -2,13 +2,27 @@ package com.example.prak2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    TextView txt1, txt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txt1 = (TextView) findViewById(R.id.textView);
+        txt2 = (TextView) findViewById(R.id.textView2);
+
+        String A = getIntent().getExtras().getString("A");
+        String B = getIntent().getExtras().getString("B");
+
+
+
+
+
     }
 }
